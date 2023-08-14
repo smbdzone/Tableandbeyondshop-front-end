192fbc4dc10b014414ac2291cae854b957f70820
Milind Nair <milindnair261003@gmail.com>
2023-08-14 23:06:17 +0530

    feat:added new arrivals on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,8 +9,114 @@ import ImageCarousel from '../../components/ImageCarousel';
 import Table from '../../components/Table';
 import Footer from '../../components/Footer';
 import CarouselSection from '../../components/Carousel';
-
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { useState } from 'react';
+import car5 from '../../assets/car5.png';
+import car2 from '../../assets/car2.png';
+import car3 from '../../assets/car3.png';
+import car4 from '../../assets/car4.png';
+import ItemCard from '../../components/ItemCard';
+// import './Home.css';
 const Home = () => {
+
+  const [gridQuantity, setgridQuantity] = useState(3);
+  const carouselItems = [
+      {
+          id: 1,
+          src: car5,
+          title: "very nice",
+          description: "very nice",
+          price: 500,
+          detail: "New",
+
+      },
+      {
+          id: 2,
+          src: car2,
+          title: "very nice",
+          description: "very nice",
+          price: 500,
+          detail: "New",
+
+      },
+      {
+          id: 3,
+          src: car3,
+          title: "very nice",
+          description: "very nice",
+          price: 500,
+          detail: "New",
+
+      },
+      {
+          id: 4,
+          src: car4,
+          title: "very nice",
+          description: "very nice",
+          price: 500,
+          detail: "New",
+
+      },
+      {
+          id: 5,
+          src: car5,
+          title: "very nice",
+          description: "very nice",
+          price: 500,
+          detail: "New",
+
+      },
+      {
+          id: 6,
+          src: car2,
+          title: "very nice",
+          description: "veryyyy nice",
+          price: 500,
+          detail: "New",
+
+      }
+      // Add more carousel items
+  ];
+
+  const itemsPerPage = 4; // Number of items to display per page
+  const [currentPage, setCurrentPage] = useState(0);
+  const totalPages = Math.ceil(carouselItems.length / itemsPerPage);
+
+  const handlePrevPage = () => {
+      setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+  };
+
+  const handleNextPage = () => {
+      setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages - 1));
+  };
+
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const slicedItems = carouselItems.slice(startIndex, endIndex);
+
+  const calculateCardSize = () => {
+      const baseWidth = 318;
+      const baseHeight = 435;
+      const reductionFactor = (gridQuantity - 1) * 0.1;
+      const width = baseWidth * (1 - reductionFactor);
+      const height = baseHeight * (1 - reductionFactor);
+
+      return { width, height };
+  };
+
+  const { width: cardWidth, height: cardHeight } = calculateCardSize();
+
+  const openModal = () => {
+      if(document.getElementsByClassName('myModal')[0].style.display === 'block'){
+          document.getElementsByClassName('myModal')[0].style.display = 'none';
+          return;
+      }else{
+          document.getElementsByClassName('myModal')[0].style.display = 'block';
+      console.log('modal opened');
+
+      }
+      
+  }
   return (
     <div>
     <Navbar />
@@ -20,6 +126,38 @@ const Home = () => {
     <CarouselSection />   
     <Display />
     <ImageCarousel />
+    <div className='text-left relative ml-[50px] mt-[60px] newArrival'>
+                    <div className='flex w-full ' style={{
+                        justifyContent: 'space-between'
+                
+                    }}>
+                    <p className='Dheading'>Last Viewed Products</p>
+                    <div className="flex justify-center ">
+                        <button
+                            onClick={handlePrevPage}
+                            disabled={currentPage === 0}
+                            className="mr-2"
+                        >
+                            <FaChevronLeft size={20} />
+                        </button>
+                        <button
+                            onClick={handleNextPage}
+                            disabled={currentPage === totalPages - 1}
+                        >
+                            <FaChevronRight size={20} />
+                        </button>
+                    </div>
+                    </div>
+                    <hr></hr>
+                    <div className="grid grid-cols-4  mt-[50px] carousel pb-[100px]">
+                        {/* Display sliced carousel items */}
+                        {slicedItems.map((item) => (
+                            <ItemCard src={item.src} title={item.title} description={item.description} price={item.price} favIconPos="lvp" bgheight="318px" bgwidth="318px" imgheight="257px" imgwidth="257px" height="100%" width="318px" />
+                        ))}
+                    </div>
+                    
+
+                </div>
     <Table />
     <Footer />
     </div>
