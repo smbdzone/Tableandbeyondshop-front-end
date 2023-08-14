b9ed6484fabac1c373b3b956b2faf59dd89bec9b
Milind Nair <milindnair261003@gmail.com>
2023-08-14 17:34:19 +0530

    feat:itemCard , dining layout finished

--- src/pages/Dining/Dining.jsx
+++ src/pages/Dining/Dining.jsx
@@ -10,11 +10,93 @@ import Footer from '../../components/Footer';
 import ItemCard from '../../components/ItemCard';
 import Rectangle1 from '../../assets/Dinnerware1.png';
 import './Dining.css';
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import car5 from '../../assets/car5.png';
+import car2 from '../../assets/car2.png';
+import car3 from '../../assets/car3.png';
+import car4 from '../../assets/car4.png';
+
 
 
 const Dining = () => {
+
+    const carouselItems = [
+        {
+            id: 1,
+            src: car5,
+            title: "very nice",
+            description: "very nice",
+            price: 500,
+            detail: "New",
+
+        },
+        {
+            id: 2,
+            src: car2,
+            title: "very nice",
+            description: "very nice",
+            price: 500,
+            detail: "New",
+
+        },
+        {
+            id: 3,
+            src: car3,
+            title: "very nice",
+            description: "very nice",
+            price: 500,
+            detail: "New",
+
+        },
+        {
+            id: 4,
+            src: car4,
+            title: "very nice",
+            description: "very nice",
+            price: 500,
+            detail: "New",
+
+        },
+        {
+            id: 5,
+            src: car5,
+            title: "very nice",
+            description: "very nice",
+            price: 500,
+            detail: "New",
+
+        },
+        {
+            id: 6,
+            src: car2,
+            title: "very nice",
+            description: "veryyyy nice",
+            price: 500,
+            detail: "New",
+
+        }
+        // Add more carousel items
+    ];
+
+    const itemsPerPage = 4; // Number of items to display per page
+    const [currentPage, setCurrentPage] = useState(0);
+    const totalPages = Math.ceil(carouselItems.length / itemsPerPage);
+
+    const handlePrevPage = () => {
+        setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+    };
+
+    const handleNextPage = () => {
+        setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages - 1));
+    };
+
+    const startIndex = currentPage * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const slicedItems = carouselItems.slice(startIndex, endIndex);
+
     return (
-        <div>
+        <div style={{overflowX:'hidden'}}>
             <Navbar />
             <MegaNav />
             <ContactBar />
@@ -64,7 +146,7 @@ const Dining = () => {
                     </div>
 
                 </div>
-                <div className="grid grid-cols-3 gap-[70px] ml-[100px] mt-[50px] ">
+                <div className="grid grid-cols-3 gap-[70px] ml-[100px] mt-[50px] carousel ">
                     {/* Item Cards */}
                     <ItemCard src={Rectangle1} title={"very nice"} description={"very nice"} price={500} detail={"New"} />
                     <ItemCard src={Rectangle1} title={"very nice"} description={"very nice"} price={500} detail={"New"} />
@@ -73,7 +155,38 @@ const Dining = () => {
 
                     {/* You can add more ItemCard components here */}
                 </div>
+                <div className='text-left relative ml-[50px]'>
+                    <div className='flex w-full ' style={{
+                        justifyContent: 'space-between'
+                
+                    }}>
+                    <p className='Dheading'>Last Viewed Products</p>
+                    <div className="flex justify-center ">
+                        <button
+                            onClick={handlePrevPage}
+                            disabled={currentPage === 0}
+                            className="mr-2"
+                        >
+                            <FaChevronLeft size={20} />
+                        </button>
+                        <button
+                            onClick={handleNextPage}
+                            disabled={currentPage === totalPages - 1}
+                        >
+                            <FaChevronRight size={20} />
+                        </button>
+                    </div>
+                    </div>
+                    <hr></hr>
+                    <div className="grid grid-cols-4  mt-[50px] carousel pb-[100px]">
+                        {/* Display sliced carousel items */}
+                        {slicedItems.map((item) => (
+                            <ItemCard src={item.src} title={item.title} description={item.description} price={item.price} favIconPos="lvp" bgheight="318px" bgwidth="318px" imgheight="257px" imgwidth="257px" height="100%" width="318px" />
+                        ))}
+                    </div>
+                    
 
+                </div>
                 <div className='flex flex-col text-left ml-[50px] gap-4'>
                     <p className='Dheading'>Dining</p>
                     <p className='DPara w-[90%]'>Dining Products: is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
